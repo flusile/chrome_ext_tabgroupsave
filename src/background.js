d0be029d8f9dbefcf1b945413bc249974aa4279c
Ulf Michaelis <ulf@sileachim.de>
2022-06-12 16:35:43 +0200

    hm, geht immer noch nicht richtig. Komisch.

--- src/background.js
+++ src/background.js
@@ -6,108 +6,109 @@ var isCreatingBackup = false;
 
 function backupNow(isAutomatic, backupID, callbackDone) 
 {
-  console.log('backupNow - isAutomatic: ' + isAutomatic + ' ID: ' + backupID);
+    console.log('backupNow - isAutomatic: ' + isAutomatic + ' ID: ' + backupID);
 
-  if (isCreatingBackup === true) 
-  {
-    console.log('backupNow - already running. Skipping..');
-    return;
-  }
-  isCreatingBackup = true;
-
-  // start work
-  // first go through all windows
-  chrome.windows.getAll({windowTypes: ['normal'], populate : true}, function (window_list) 
-  {
-    var backupListItem = {id: backupID, name: null};
-
-    var fullBackup = {
-      windows: [],
-      isAutomatic: (isAutomatic !== false),
-      totNumTabs: 0
-    };
-
-    var windowToBackup, windowTabs, bkpWindow;
-    var tab, bkpTab;
-
-    for (var i = 0; i < window_list.length; i++) 
+    if (isCreatingBackup === true) 
     {
-      console.log ('backupNow Window #' + i);
+        console.log('backupNow - already running. Skipping..');
+        return;
+    }
+    isCreatingBackup = true;
 
-      windowToBackup = window_list[i];
-      windowTabs     = windowToBackup.tabs;
-      bkpWindow = {
-        incognito: !!windowToBackup.incognito,
-        tabs: [],
-        groups: [],
-      };
+    // start work
+    // first go through all windows
+    chrome.windows.getAll({windowTypes: ['normal'], populate : true}, function (window_list) 
+    {
+        var backupListItem = {id: backupID, name: null};
 
-      for (var j = 0; j < windowTabs.length; j++) 
-      {
-        tab = windowTabs[j];
-
-        console.log('==> Tab ' + j + ' (' + tab.index + '): ' + tab.url);
-
-        bkpTab = {
-          url: tab.url,
-          title: tab.title,
-          grp: tab.groupId,
+        var fullBackup = {
+            windows: [],
+            isAutomatic: (isAutomatic !== false),
+            totNumTabs: 0
         };
 
-        // Add tab to tabs arrays
-        bkpWindow.tabs.push(bkpTab);
+        var windowToBackup, windowTabs, bkpWindow;
+        var tab, bkpTab;
 
-        if (tab.groupId != chrome.tabGroups.TAB_GROUP_ID_NONE)
+        for (var i = 0; i < window_list.length; i++) 
         {
-            chrome.tabGroups.get(tab.groupId, function (tabGroup)
+            console.log ('backupNow Window #' + i);
+
+            windowToBackup = window_list[i];
+            windowTabs     = windowToBackup.tabs;
+            bkpWindow = {
+                incognito: !!windowToBackup.incognito,
+                tabs: [],
+                groupIds: [],
+                groups: [],
+            };
+
+            for (var j = 0; j < windowTabs.length; j++) 
             {
-                // so, dass ist ja schon wieder eine async Funktion
-                // Wie kann ich hier auf das Ergebnis "warten", denn bevor die tabGroup-Daten nicht vorliegen, kann ich nicht weiter machen.
-                bkpWindow.groups.push(tabGroup);
+                tab = windowTabs[j];
+
+                console.log('==> Tab ' + j + ' (' + tab.index + '): ' + tab.url);
+
+                bkpTab = {
+                    url: tab.url,
+                    title: tab.title,
+                    grp: tab.groupId,
+                };
+
+                if (tab.groupId != chrome.tabGroups.TAB_GROUP_ID_NONE)
+                {
+                    bkpWindow.groupIds[tab.groupId] = tab.groupId;
+                }
+
+                // Add tab to tabs arrays
+                bkpWindow.tabs.push(bkpTab);
+            }
+
+            // no try to get group details from async source
+            var operations = [];
+            for (var gid in bkpWindow.groupIds) 
+            {
+                operations[gid] = chrome.tabGroups.get(bkpWindow.groupIds[gid]);
+            }
+            console.log('waiting for operations');
+            Promise.all(operations).then(() => {
+                console.log('all operations completed 1');
+                for (var gid in operations) 
+                {
+                    console.log('got details for group id ' + gid);
+                    console.log(operations[gid]);
+                }
+                console.log('saving fullbackup');
+                fullBackup.totNumTabs += windowTabs.length;
+                fullBackup.windows.push(bkpWindow);
+            }).catch(() => {
+                console.log('oops, catch');
+                //console.log(reason);
             });
+            console.log('all operations completed 2');
         }
-      }
 
-      fullBackup.totNumTabs += windowTabs.length;
-      fullBackup.windows.push(bkpWindow);
-    }
+        /*
+        var backupListItemArray, fullBackupArray, callbackDoneWrapper;
+        backupListItemArray = [backupListItem];
+        fullBackupArray     = [fullBackup];
+        callbackDoneWrapper = function(isSuccess, backupList, storageSetValues) {
+        isCreatingBackup = false;
 
-    /*
-    var backupListItemArray, fullBackupArray, callbackDoneWrapper;
-    backupListItemArray = [backupListItem];
-    fullBackupArray     = [fullBackup];
-    callbackDoneWrapper = function(isSuccess, backupList, storageSetValues) {
-      isCreatingBackup = false;
+        if (isSuccess) {
+            updateBrowserActionIcon (0);
+            callbackDone(true, backupListItem, fullBackup);
+        }
+        else {
+            updateBrowserActionIcon (1);
+            callbackDone(false);
+        }
+        }
 
-      if (isSuccess) {
-        updateBrowserActionIcon (0);
-        callbackDone(true, backupListItem, fullBackup);
-      }
-      else {
-        updateBrowserActionIcon (1);
-        callbackDone(false);
-      }
-    }
-
-    saveBackups (backupListItemArray, fullBackupArray, callbackDoneWrapper);
-    */
-  });
-
-
-
-
-
-
-
+        saveBackups (backupListItemArray, fullBackupArray, callbackDoneWrapper);
+        */
+    });
 }
-
-
-
-
-
-
-
-
 
 
 
